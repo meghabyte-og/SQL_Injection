00303f26a764e530fa00ca9e731d2f7b9121fbe9
Megha <135510418+meghabyte-og@users.noreply.github.com>
2023-08-07 08:48:50 +0530

    Update sqlstatements.sql

--- sqlstatements.sql
+++ sqlstatements.sql
@@ -1,5 +1,5 @@
 
 SELECT * FROM users WHERE username = '$input_username' AND password = '$input_password';
-SELECT * FROM users WHERE username = 'admin'' AND password = 'password';
 SELECT * FROM users WHERE username = 'admin' OR '1'='1' AND password = '$input_password';
 SELECT * FROM users WHERE username = 'admin' -- AND password = 'password'
+SELECT * FROM users WHERE username = 'admin'' AND password = 'password';
